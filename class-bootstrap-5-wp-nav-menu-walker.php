b5731a82312851377b9d1b23406c506f11c2d058
Nishant Vaity <enishant@users.noreply.github.com>
2023-12-09 01:44:15 +0530

    Updated class according to WordPress standard

--- class-bootstrap-5-wp-nav-menu-walker.php
+++ class-bootstrap-5-wp-nav-menu-walker.php
@@ -1,79 +1,119 @@
 <?php
-// bootstrap 5 wp_nav_menu walker
-class bootstrap_5_wp_nav_menu_walker extends Walker_Nav_menu
-{
-  private $current_item;
-  private $dropdown_menu_alignment_values = [
-    'dropdown-menu-start',
-    'dropdown-menu-end',
-    'dropdown-menu-sm-start',
-    'dropdown-menu-sm-end',
-    'dropdown-menu-md-start',
-    'dropdown-menu-md-end',
-    'dropdown-menu-lg-start',
-    'dropdown-menu-lg-end',
-    'dropdown-menu-xl-start',
-    'dropdown-menu-xl-end',
-    'dropdown-menu-xxl-start',
-    'dropdown-menu-xxl-end'
-  ];
+/**
+ * Nav Menu Walker
+ *
+ * @package Bootstrap_5_Wp_Nav_Menu_Walker
+ */
 
-  function start_lvl(&$output, $depth = 0, $args = null)
-  {
-    $dropdown_menu_class[] = '';
-    foreach($this->current_item->classes as $class) {
-      if(in_array($class, $this->dropdown_menu_alignment_values)) {
-        $dropdown_menu_class[] = $class;
-      }
-    }
-    $indent = str_repeat("\t", $depth);
-    $submenu = ($depth > 0) ? ' sub-menu' : '';
-    $output .= "\n$indent<ul class=\"dropdown-menu$submenu " . esc_attr(implode(" ",$dropdown_menu_class)) . " depth_$depth\">\n";
-  }
+/**
+ * Bootstrap 5 wp_nav_menu walker
+ *
+ * Bootstrap 5 WordPress navigation menu walker
+ */
+class Bootstrap_5_Wp_Nav_Menu_Walker extends Walker_Nav_menu {
+	/**
+	 * Current item
+	 * 
+	 * @var string Current item
+	 */
+	private $current_item;
 
-  function start_el(&$output, $item, $depth = 0, $args = null, $id = 0)
-  {
-    $this->current_item = $item;
+	/**
+	 * Dropdown menu
+	 * 
+	 * @var array Dropdown menu alignment values.
+	 */
+	private $dropdown_menu_alignment_values = array(
+		'dropdown-menu-start',
+		'dropdown-menu-end',
+		'dropdown-menu-sm-strt',
+		'dropdown-menu-sm-end',
+		'dropdown-menu-md-start',
+		'dropdown-menu-md-end',
+		'dropdown-menu-lg-start',
+		'dropdown-menu-lg-end',
+		'dropdown-menu-xl-start',
+		'dropdown-menu-xl-end',
+		'dropdown-menu-xxl-start',
+		'dropdown-menu-xxl-end',
+	);
 
-    $indent = ($depth) ? str_repeat("\t", $depth) : '';
+	/**
+	 * Starts the list before the elements are added.
+	 *
+	 * @param string   $output Used to append additional content (passed by reference).
+	 * @param int      $depth  Depth of menu item. Used for padding.
+	 * @param stdClass $args   An object of wp_nav_menu() arguments.
+	 */
+	public function start_lvl( &$output, $depth = 0, $args = null ) {
+		$dropdown_menu_class[] = '';
+		foreach ( $this->current_item->classes as $class ) {
+			if ( in_array( $class, $this->dropdown_menu_alignment_values, true ) ) {
+				$dropdown_menu_class[] = $class;
+			}
+		}
+		$indent  = str_repeat( "\t", $depth );
+		$submenu = ( $depth > 0 ) ? ' sub-menu' : '';
+		$output .= "\n$indent<ul class=\"dropdown-menu$submenu " . esc_attr( implode( ' ', $dropdown_menu_class ) ) . " depth_$depth\">\n";
+	}
 
-    $li_attributes = '';
-    $class_names = $value = '';
+	/**
+	 * Starts the element output.
+	 *
+	 * @param string   $output            Used to append additional content (passed by reference).
+	 * @param WP_Post  $item              Menu item data object.
+	 * @param int      $depth             Depth of menu item. Used for padding.
+	 * @param stdClass $args              An object of wp_nav_menu() arguments.
+	 * @param int      $id                Optional. ID of the current menu item. Default 0.
+	 */
+	public function start_el( &$output, $item, $depth = 0, $args = null, $id = 0 ) {
+		$this->current_item = $item;
 
-    $classes = empty($item->classes) ? array() : (array) $item->classes;
+		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
 
-    $classes[] = ($args->walker->has_children) ? 'dropdown' : '';
-    $classes[] = 'nav-item';
-    $classes[] = 'nav-item-' . $item->ID;
-    if ($depth && $args->walker->has_children) {
-      $classes[] = 'dropdown-menu dropdown-menu-end';
-    }
+		$li_attributes = '';
 
-    $class_names =  join(' ', apply_filters('nav_menu_css_class', array_filter($classes), $item, $args));
-    $class_names = ' class="' . esc_attr($class_names) . '"';
+		$value       = '';
+		$class_names = '';
 
-    $id = apply_filters('nav_menu_item_id', 'menu-item-' . $item->ID, $item, $args);
-    $id = strlen($id) ? ' id="' . esc_attr($id) . '"' : '';
+		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
 
-    $output .= $indent . '<li ' . $id . $value . $class_names . $li_attributes . '>';
+		$classes[] = ( $args->walker->has_children ) ? 'dropdown' : '';
+		$classes[] = 'nav-item';
+		$classes[] = 'nav-item-' . $item->ID;
+		
+		if ( $depth && $args->walker->has_children ) {
+			$classes[] = 'dropdown-menu dropdown-menu-end';
+		}
 
-    $attributes = !empty($item->attr_title) ? ' title="' . esc_attr($item->attr_title) . '"' : '';
-    $attributes .= !empty($item->target) ? ' target="' . esc_attr($item->target) . '"' : '';
-    $attributes .= !empty($item->xfn) ? ' rel="' . esc_attr($item->xfn) . '"' : '';
-    $attributes .= !empty($item->url) ? ' href="' . esc_attr($item->url) . '"' : '';
+		$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+		$class_names = ' class="' . esc_attr( $class_names ) . '"';
 
-    $active_class = ($item->current || $item->current_item_ancestor || in_array("current_page_parent", $item->classes, true) || in_array("current-post-ancestor", $item->classes, true)) ? 'active' : '';
-    $nav_link_class = ( $depth > 0 ) ? 'dropdown-item ' : 'nav-link ';
-    $attributes .= ( $args->walker->has_children ) ? ' class="'. $nav_link_class . $active_class . ' dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"' : ' class="'. $nav_link_class . $active_class . '"';
+		$id = apply_filters( 'nav_menu_item_id', 'menu-item-' . $item->ID, $item, $args );
+		$id = strlen( $id ) ? ' id="' . esc_attr( $id ) . '"' : '';
 
-    $item_output = $args->before;
-    $item_output .= '<a' . $attributes . '>';
-    $item_output .= $args->link_before . apply_filters('the_title', $item->title, $item->ID) . $args->link_after;
-    $item_output .= '</a>';
-    $item_output .= $args->after;
+		$output .= $indent . '<li ' . $id . $value . $class_names . $li_attributes . '>';
 
-    $output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
-  }
+		$attributes  = ! empty( $item->attr_title ) ? ' title="' . esc_attr( $item->attr_title ) . '"' : '';
+		$attributes .= ! empty( $item->target ) ? ' target="' . esc_attr( $item->target ) . '"' : '';
+		$attributes .= ! empty( $item->xfn ) ? ' rel="' . esc_attr( $item->xfn ) . '"' : '';
+		$attributes .= ! empty( $item->url ) ? ' href="' . esc_attr( $item->url ) . '"' : '';
+
+		$active_class = ( $item->current || $item->current_item_ancestor || in_array( 'current_page_parent', $item->classes, true ) || in_array( 'current-post-ancestor', $item->classes, true ) ) ? 'active' : '';
+
+		$nav_link_class = ( $depth > 0 ) ? 'dropdown-item ' : 'nav-link ';
+		
+		$attributes .= ( $args->walker->has_children ) ? ' class="' . $nav_link_class . $active_class . ' dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"' : ' class="' . $nav_link_class . $active_class . '"';
+
+		$item_output  = $args->before;
+		$item_output .= '<a' . $attributes . '>';
+		$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+		$item_output .= '</a>';
+		$item_output .= $args->after;
+
+		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	}
 }
-// register a new menu
-register_nav_menu('main-menu', 'Main menu');
+
+// register a new menu.
+register_nav_menu( 'main-menu', 'Main menu' );
